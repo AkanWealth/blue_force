68aa6642385978d1a37452a3fc87e8d9c7759427
Akan Asanga <akanasanga@outlook.com>
2021-10-05 12:26:53 +0100

    testing heroku

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const appiontment = require("./controllers/appointmentsController")
 const authorization = require("./middlewares/authorization");
 
 module.exports = (app) => {
+    app.get("/"), (req, res) => {
+        res.send("Welcome on board");
+    };
     app.get("/users", authorization, usersController.getAllUsers);
 
     app.post("/login", usersController.login);
